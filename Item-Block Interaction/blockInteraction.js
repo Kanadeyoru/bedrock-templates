8837c2a8ebb3670054fc160d241bd6c2b00fb4bf
Kanadeyoru <kanadeyoru0@gmail.com>
2024-11-07 20:36:14 -0300

    Add files via upload

---
+++ Item-Block Interaction/blockInteraction.js
@@ -0,0 +1,107 @@
+import { world, EquipmentSlot, EntityEquippableComponent, system, ItemDurabilityComponent } from '@minecraft/server';
+import { entryBlocks, excludedPermutations } from './blocksList.js';
+
+const processedBlocks = {};
+const PROCESS_TIME_WINDOW = 20;
+const entryBlocksMap = new Map();
+
+entryBlocks.forEach((blockData, key) => {
+    if (blockData.inputBlock) {
+        if (!entryBlocksMap.has(blockData.inputBlock)) {
+            entryBlocksMap.set(blockData.inputBlock, []);
+        }
+        entryBlocksMap.get(blockData.inputBlock).push(blockData);
+    }
+});
+
+world.beforeEvents.itemUseOn.subscribe((eventData) => {
+    const block = eventData.block;
+    const player = eventData.source;
+    const face = eventData.blockFace;
+    const mainhand = player.getComponent(EntityEquippableComponent.componentId)?.getEquipmentSlot(EquipmentSlot.Mainhand);
+    const item = mainhand?.getItem();
+    const { x, y, z } = block.location;
+    const blockKey = `${x}, ${y}, ${z}`;
+    const lastProcessedTime = processedBlocks[blockKey];
+
+    if (!mainhand || !item) return;
+
+    if (lastProcessedTime && Date.now() - lastProcessedTime < PROCESS_TIME_WINDOW) {
+        return;
+    }
+
+    processedBlocks[blockKey] = Date.now();
+    const matchingConfigs = entryBlocks.get(block.typeId);
+
+    if (matchingConfigs && face !== "Down") {
+
+        matchingConfigs.forEach((matchingBlock) => {
+            const expectedStates = Object.fromEntries(matchingBlock.inputBlockPermutations.map(perm => [perm.key, perm.value]));
+
+            if (!block.matches || !block.matches(matchingBlock.inputBlock, expectedStates)) {
+                return;
+            }
+
+            if (matchingBlock.itemTag && mainhand.hasTag(matchingBlock.itemTag)) {
+                system.run(() => {
+                    const oldBlock = block;
+                    const inputBlockPermutations = oldBlock.permutation.getAllStates();
+                    block.setType(matchingBlock.outputBlock);
+                    const newBlock = block;
+
+                    if (newBlock) {
+                        let newBlockPermutation = newBlock.permutation;
+                        const outputBlockPermutations = newBlock.permutation.getAllStates();
+
+                        matchingBlock.outputBlockPermutations.forEach(({ key, value }) => {
+                            newBlockPermutation = newBlockPermutation.withState(key, value);
+                        });
+
+                        for (let key in inputBlockPermutations) {
+                            const outputHasKey = matchingBlock.outputBlockPermutations.some(perm => perm.key === key);
+
+                            if (!outputHasKey && outputBlockPermutations.hasOwnProperty(key) && (!excludedPermutations.has(key) || matchingBlock.inputBlockPermutations.some(perm => perm.key === key) || matchingBlock.outputBlockPermutations.some(perm => perm.key === key))) {
+                                newBlockPermutation = newBlockPermutation.withState(key, inputBlockPermutations[key]);
+                            }
+                        }
+
+                        newBlock.setPermutation(newBlockPermutation);
+                    }
+                    player.playSound(matchingBlock.interactionSound, { location: block });
+                    block.dimension.spawnParticle(matchingBlock.interactionParticle, { x: x + 0.5, y: y + 0.75, z: z + 0.5 });
+                    reduceItem(player, mainhand, item, matchingBlock.itemType);
+                });
+            }
+        });
+    }
+    system.run(() => {
+        delete processedBlocks[blockKey];
+    });
+});
+
+function reduceItem(player, mainhand, item, itemType) {
+    if (item.amount <= 1) return undefined;
+    if (itemType === "Durability") {
+        mainhand.setItem(reduceDurability(player, item));
+    } else if (itemType === "Stackable") {
+        mainhand.setItem(reduceStack(player, item));
+    }
+}
+
+function reduceStack(player, item) {
+    if (player.getGameMode() === "creative") return item;
+    item.amount -= 1;
+    return item;
+}
+
+function reduceDurability(player, item) {
+    if (player.getGameMode() === "creative") return item;
+    const comp = item.getComponent(ItemDurabilityComponent.componentId);
+    if (!comp) return item;
+    comp.damage += 1;
+    if (comp.damage >= comp.maxDurability) {
+        world.playSound("random.break", player.location);
+        return undefined;
+    }
+    return item;
+}
---
+++ Item-Block Interaction/blocksList.js
@@ -0,0 +1,77 @@
+// Permutations to ignore. If an ignored permutation is specified inside 'inputBlockPermutations' or 'outputBlockPermutations', it will be forcefully used.
+export const excludedPermutations = new Set([
+    "wood_type",
+    "top_slot_bit",
+    "weirdo_direction"
+]);
+
+export const entryBlocks = new Map([
+    [
+        "minecraft:acacia_stairs", // Required: Insert here the same value as 'inputBlock'
+        [
+            {
+                inputBlock: "minecraft:acacia_stairs", // Required: The identifier of the block that will be interacted
+                inputBlockPermutations: [
+                    { key: "weirdo_direction", value: 1 },
+                    { key: "upside_down_bit", value: true }
+                ], // Optional: If you want to specify permutations for the 'inputBlock', add them here.
+                outputBlock: "minecraft:oak_stairs", // Required: The identifier of the block that will be set after the interaction
+                outputBlockPermutations: [
+                    { key: "weirdo_direction", value: 2 },
+                    { key: "upside_down_bit", value: true }
+                ], // Optional: If you don't specify permutations for the 'outputBlock' and the 'outputBlock' shares one or more permutations with the 'inputBlock', their values will be preserved during the interaction.
+                interactionParticle: "minecraft:villager_angry", // Optional: The particle identifier, if none leave ""
+                interactionSound: "use.stone", // Optional: The sound identifier, if none leave ""
+                itemTag: "minecraft:is_sword", // Required: The tag that will be used to indentify the item(s) used in this interaction
+                itemType: "Durability" // Optional: The type of consumption of your item, values accepted are "Stackable", "Durability" or ""
+            },
+            {
+                // In case the same block will have more than one interaction:
+                inputBlock: "minecraft:acacia_stairs",
+                inputBlockPermutations: [
+                    { key: "weirdo_direction", value: 1 },
+                    { key: "upside_down_bit", value: false }
+                ],
+                outputBlock: "minecraft:oak_stairs",
+                outputBlockPermutations: [
+                    { key: "weirdo_direction", value: 1 },
+                    { key: "upside_down_bit", value: true }
+                ],
+                interactionParticle: "minecraft:villager_angry",
+                interactionSound: "use.stone",
+                itemTag: "minecraft:is_sword",
+                itemType: "Durability"
+            }
+        ]
+    ],
+    // Add more blocks for more interactions
+    [
+        "minecraft:oak_slab",
+        [
+            {
+                inputBlock: "minecraft:oak_slab",
+                inputBlockPermutations: [
+                    { key: "minecraft:vertical_half", value: "bottom" },
+                ],
+                outputBlock: "minecraft:birch_slab",
+                outputBlockPermutations: [],
+                interactionParticle: "minecraft:crop_growth_emitter",
+                interactionSound: "dig.dirt",
+                itemTag: "minecraft:coals",
+                itemType: "Stackable"
+            },
+            {
+                inputBlock: "minecraft:oak_slab", // Required: The identifier of the block that will be interacted
+                inputBlockPermutations: [
+                    { key: "minecraft:vertical_half", value: "top" },
+                ],
+                outputBlock: "minecraft:birch_slab",
+                outputBlockPermutations: [],
+                interactionParticle: "minecraft:crop_growth_emitter",
+                interactionSound: "dig.dirt",
+                itemTag: "minecraft:coals",
+                itemType: "Stackable"
+            }
+        ]
+    ]
+]);
